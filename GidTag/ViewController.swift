256339a083314d260527fb98b5d3a95b71be33de
“pickle-debug” <1941109171@qq.com>
2024-03-27 15:59:04 +0800

    Initial Commit

---
+++ GidTag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GidTag
+//
+//  Created by Tanshuo on 2024/3/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
